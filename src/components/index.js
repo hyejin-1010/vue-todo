f3316007355680afb6452a2f857b513ea39aaa1c
Hyejin1010 <hyepago@naver.com>
2020-12-10 23:33:22 +0900

    컴포넌트 생성 및 등록하기

---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+import TodoHeader from './TodoHeader'
+import TodoInput from './TodoInput'
+import TodoList from './TodoList'
+import TodoFooter from './TodoFooter'
+
+export { TodoHeader, TodoInput, TodoList, TodoFooter }
